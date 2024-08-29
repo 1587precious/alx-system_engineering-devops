eb7de1013d2f48c7bc211645bf25a6972b6e8437
1587precious <preciousconstance7@gmail.com>
2024-08-29 13:24:56 +0000

    task 0

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,8 @@
+# fix the limit nginx.
+exec { 'myfix':
+command => 'sed -i "/ULIMIT=/c\ULIMIT=\"-n 2000\"" /etc/default/nginx',
+path    => '/bin',
+}
+service { 'nginx':
+ensure    => running,
+subscribe => Exec['myfix'],
